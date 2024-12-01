a927f9fb7f4ef92cfd7f07f28437151fbf5a531f
stefanRachev <starfystef@gmail.com>
2024-12-01 20:48:37 +0200

    add github provider

--- src/auth.js
+++ src/auth.js
@@ -10,6 +10,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+
+    GitHibProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      params: {
+        prompt: "consent",
+        access_type: "offline",
+        response_type: "code",
+      },
     }),
   ],
 });
