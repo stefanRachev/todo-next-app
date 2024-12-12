a6358eb5b93ba001e56cad6f1c3628c32065ffa8
stefanRachev <starfystef@gmail.com>
2024-12-12 21:03:45 +0200

    refactor .env

--- src/auth.js
+++ src/auth.js
@@ -10,8 +10,8 @@ import connectToDatabase from "./lib/mongoDB";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      clientId: process.env.NEXT_PUBLIC_AUTH_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_AUTH_GOOGLE_SECRET,
       authorization: {
         params: {
           prompt: "consent",
