dba95ab89b83cb76c97a981e94e65996877754af
stefanRachev <starfystef@gmail.com>
2024-12-24 14:10:12 +0200

    refactor product schema user to String type

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,8 +3,7 @@ import mongoose, { Schema } from "mongoose";
 
 const productSchema = new Schema({
   user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    type: String,
     required: true,
   },
   productName: {
