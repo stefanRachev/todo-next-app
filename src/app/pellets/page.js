a22241b0dcbaa84df55f096f9e3d238e746d01e4
stefanRachev <starfystef@gmail.com>
2025-01-17 13:58:39 +0200

    Fixed initial render issue in PelletsPage by adding conditional rendering based on session status

--- src/app/pellets/page.js
+++ src/app/pellets/page.js
@@ -1,68 +1,66 @@
-  //src/app/pellets/page.js
-  "use client";
+//src/app/pellets/page.js
+"use client";
 
-  import { useSession } from "next-auth/react";
-  import { useRouter } from "next/navigation";
-  import { fetchPellets } from "./utils/apiUtils";
-  import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { fetchPellets } from "./utils/apiUtils";
+import { useState, useEffect } from "react";
 
-  import PelletsForm from "./components/PelletsForm";
-  import PelletsList from "./components/PelletList";
+import PelletsForm from "./components/PelletsForm";
+import PelletsList from "./components/PelletList";
 
-  export default function PelletsPage() {
-    const [pelletsData, setPelletsData] = useState([]);
-    const [isDataChanged, setIsDataChanged] = useState(true);
+export default function PelletsPage() {
+  const [pelletsData, setPelletsData] = useState([]);
+  const [isDataChanged, setIsDataChanged] = useState(true);
 
-    const { data: session, status } = useSession();
-    const router = useRouter();
-    const accessToken = session?.user?.accessToken;
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const accessToken = session?.user?.accessToken;
 
-    useEffect(() => {
-      if (isDataChanged) {
-        fetchPellets(accessToken).then((data) => {
+  useEffect(() => {
+    if (status === "unauthenticated" || !accessToken) {
+      router.push("/login");
+    }
+  }, [accessToken, status, router]);
+
+  useEffect(() => {
+    if (isDataChanged && accessToken) {
+      fetchPellets(accessToken)
+        .then((data) => {
           setPelletsData(data);
           setIsDataChanged(false);
+        })
+        .catch((error) => {
+          console.error("Неуспешно зареждане на данните:", error.message);
         });
-      }
-    }, [isDataChanged, accessToken]);
-
-    // const onPelletAdded = async () => {
-    //   try {
-    //     const updatedPellets = await fetchPellets(accessToken); 
-    //     setPelletsData(updatedPellets); 
-    //   } catch (error) {
-    //     console.error("Грешка при обновяване на пелетите:", error);
-    //   }
-    // };
-
-    const handlePelletAdded = () => {
-      setIsDataChanged(true);
-    };
-
-    if (status === "unauthenticated") {
-      router.push("/login");
-      return null;
     }
+  }, [isDataChanged, accessToken]);
 
-    if (status === "loading") {
-      return <div>Зареждане...</div>;
-    }
+  const handlePelletAdded = () => {
+    setIsDataChanged(true);
+  };
 
-    return (
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-        <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
-          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
-            Въвеждане на пелети за отопление
-          </h1>
+ 
 
-          <div className="space-y-4">
-            <PelletsForm
-              accessToken={accessToken}
-              onPelletAdded={handlePelletAdded}
-            />
-            <PelletsList pelletsData={pelletsData} />
-          </div>
+  if (status === "loading" || status === "unauthenticated") {
+    return <div>Зареждане...</div>;
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
+        <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+          Въвеждане на пелети за отопление
+        </h1>
+
+        <div className="space-y-4">
+          <PelletsForm
+            accessToken={accessToken}
+            onPelletAdded={handlePelletAdded}
+          />
+          <PelletsList pelletsData={pelletsData} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
