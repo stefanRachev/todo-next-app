a0828944c7f27c377a75152e9da1c94fbea0f94c
stefanRachev <starfystef@gmail.com>
2025-01-22 18:55:27 +0200

    redoing the footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
 
-
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 mt-4">
-      <div className="container mx-auto px-4 flex justify-between items-center">
-        <span>© 2025 MyApp. Всички права запазени.</span>
-        <div className="flex space-x-4">
-          <Link href="/terms" className="hover:underline">
+    <footer className="bg-gray-800 text-white py-4 mt-auto fixed bottom-0 w-full">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+        <div className="flex flex-col md:flex-row space-x-0 md:space-x-6 justify-start md:justify-center">
+          <Link href="/terms" className="block py-2 px-4 hover:underline">
             Условия за ползване
           </Link>
-          <Link href="/privacy-policy" className="hover:underline">
+          <Link
+            href="/privacy-policy"
+            className="block py-2 px-4 hover:underline mt-2 md:mt-0"
+          >
             Политика за поверителност
           </Link>
-          <Link href="/contact" className="hover:underline">
+          <Link
+            href="/contact"
+            className="block py-2 px-4 hover:underline mt-2 md:mt-0"
+          >
             Контакт
           </Link>
         </div>
@@ -23,3 +27,4 @@ function Footer() {
 }
 
 export default Footer;
+
