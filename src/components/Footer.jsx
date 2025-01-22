64b9e71aa25f76031ff977c8f4097639684758e0
stefanRachev <starfystef@gmail.com>
2025-01-22 21:04:47 +0200

    change in footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 mt-auto fixed bottom-0 w-full">
+    <footer className="bg-gray-800 text-white py-4 mt-auto w-full">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
         <div className="flex flex-col md:flex-row space-x-0 md:space-x-6 justify-start md:justify-center">
           <Link href="/terms" className="block py-2 px-4 hover:underline">
